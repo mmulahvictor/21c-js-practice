f0dbf60c1f3300bc0784b4c3dcac59d581d767ba
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 01:38:23 +0300

    js datatypes and objects

--- index.js
+++ index.js
@@ -3,3 +3,39 @@
 let name = "John";
 let age = 30;
 age = 30 + 1;
+
+//Data types
+//String
+let lastName = "Peter";
+
+//Number
+let ageJohn = 30;
+
+//Boolean
+let fullAge = true;
+//undefined
+let x;
+
+//null
+let y = null;
+
+//object
+let z = {}
+
+//array
+let a = [1, 2, 3, 4, 5];
+
+//function
+function add(a, b){
+    return a + b;
+}
+
+//typeof
+console.log(typeof ageJohn);
+
+//object
+let person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25
+}
