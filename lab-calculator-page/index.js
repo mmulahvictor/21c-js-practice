6bf2c8e3d1840db91a6f2874fcae18185b6f4c37
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 02:38:46 +0300

    display output for the prompt

--- lab-calculator-page/index.js
+++ lab-calculator-page/index.js
@@ -1,3 +1,12 @@
+//user input
 let name = prompt("what is your name?");
 let num1 = prompt("what is your first number?");
 let num2 = prompt("what is your second number?");
+
+//output
+document.write(`<h1>Welcome to Number Fun, ${name}</h1>`);
+document.write(`Operant#1: ${num1} Oprant#2: ${num2}`);
+document.createElement("div");
+
+
+//operations
