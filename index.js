8fe274cee39b2a73f4f55559f1fd13df0e02aae1
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 02:08:23 +0300

    perfom arithmetic operations

--- index.js
+++ index.js
@@ -42,3 +42,27 @@ let person = {
 
 //accessing object
 console.log(person.firstName);
+
+//arithmetic operators
+let x1 = 10;
+let y1 = 3;
+
+//addition
+let z1 = x1 + y1;
+let greet = "Hello, " + person.firstName + " " + person.lastName + " " + age + " years old";
+console.log(greet);
+
+//subtraction
+z1 = x1 - y1;
+
+//multiplication
+z1 = x1 * y1;
+
+//division
+z1 = x1 / y1;
+
+//modulus
+z1 = x1 % y1;
+
+//power
+z1 = x1 ** y1;
