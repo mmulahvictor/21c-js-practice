c45afebe26f3337c55b8b05a65ef2f25d948bf94
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 13:58:46 +0300

    examples of loops

---
+++ loops/index.js
@@ -0,0 +1,25 @@
+//loops
+//while loop
+while (condition) {
+    //code to execute
+}
+
+//for loop
+for (let i = 0; i < 10; i++) {
+    //code to execute
+}
+
+//for in loop
+for (let i in array) {
+    //code to execute
+}
+
+//for of loop
+for (const iterator of object) {
+    //code to execute
+}
+
+//forEach loop
+array.forEach(function (item){
+    //code to execute
+})
