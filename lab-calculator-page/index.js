7ce316fd218f26522ab6dbbdd43ab8e34c273f6c
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 03:14:31 +0300

    perfom the arithmetic calculations and display

--- lab-calculator-page/index.js
+++ lab-calculator-page/index.js
@@ -5,8 +5,20 @@ let num2 = prompt("what is your second number?");
 
 //output
 document.write(`<h1>Welcome to Number Fun, ${name}</h1>`);
-document.write(`Operant#1: ${num1} Oprant#2: ${num2}`);
-document.createElement("div");
+document.write(`<p>Operant#1: ${num1} Operant#2: ${num2}</span></p>`);
 
+//calculations
+//additon
+document.write(`<p>The sum of <span>${num1} and ${num2} is ${parseInt(num1) + parseInt(num2)}</span></p>`);
 
-//operations
+//subtraction
+document.write(`<p>The difference of <span>${num1} and ${num2} is ${parseInt(num1) - parseInt(num2)}</span></p>`);
+
+//multiplication
+document.write(`<p>The product of <span>${num1} and ${num2} is ${parseInt(num1) * parseInt(num2)}</span></p>`);
+
+//divition
+document.write(`<p>The quotient of <span>${num1} and ${num2} is ${parseInt(num1) / parseInt(num2)}</span></p>`);
+
+//modulus
+document.write(`<p>The result of the mod operation on ${num1} and ${num2} is <span>${parseInt(num1) % parseInt(num2)}</span></p>`);
