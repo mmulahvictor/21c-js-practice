e648e6d04ca9b686dd83d42bb354a9e75393dd3a
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 19:50:44 +0300

    allow user to guess 5 times

--- guesing-game/index.js
+++ guesing-game/index.js
@@ -2,17 +2,25 @@
 let number = 4
 let guess;
 let guesses = 5;
+let won = false;
 
 for(i = 0; i < guesses; i++){
 //prompt user for their guess
 guess = prompt("Guess a number");
 //if user guess is correct, congratulate them
+
 if (guess == number) {
     document.write("<h1>Correct! You won!</h1>");
+    won = true;
     break;
 }
+
 //if user guess is incorrect, let them know
 else {
     alert("incorrect. \nYou have " + (guesses - i) + " guesses left.");
 }
+}
+//if user runs out of guesses, let them know the correct answer
+if (!won) {
+    document.write("<h1>Sorry, you lost. The correct answer was " + number + ".</h1>");
 }
