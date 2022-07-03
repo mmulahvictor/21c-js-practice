41898497f180e2fa3f39dee829c84f8669dfdbf7
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 13:25:44 +0300

    logical operators and nested if-else statement

--- conditions/index.js
+++ conditions/index.js
@@ -17,4 +17,28 @@ if (condition){
 //==, ===, !=, >, <, >=, <=
 //conditions test for equality(true or false)
 
+
+//logical operators
+//&&, ||, !
+//&& - and
+if (x >= 10 && x <= 20) {
+    document.write('x is between 10 and 20');
+}
+//|| - or
+if (x >= 10 || x <= 20) {
+    document.write('x is between 10 and 20');
+}
+//! - not
+if (!x) {
+    document.write('x is not defined');
+}
+
 //nested if-else statements
+if (condito) {
+    if (condition){
+        //do something
+    }
+    
+} else {
+    
+}
