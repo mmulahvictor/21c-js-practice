d35c28c66cae2ed3d525ee7e67b63d5f69dfe9a6
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 19:11:24 +0300

    js functions

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+//functions
+//function naming convention:
+// - camelCase
+// - start with a lowercase letter
+// - use underscores to separate words
+// - use hyphens to separate words
+// - use capital letters to separate words
+function functionName(parameter) {}
+return functionName;
+
+//examples
+function sayHello(){
+    console.log('Hello');
+}
+sayHello();
