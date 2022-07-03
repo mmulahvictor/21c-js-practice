d19c4512eba9c5f3fcdd0247caba96545a0f1f1a
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 20:39:17 +0300

    validate user input

--- guesing-game/index.js
+++ guesing-game/index.js
@@ -1,12 +1,15 @@
 //program data:
-let number = 4
+let number = Math.floor(Math.random() * 100);
 let guess;
-let guesses = 5;
+let limit = 5;
 let won = false;
+let guesses = [];
 
-for(i = 0; i < guesses; i++){
+for(i = 0; i < limit; i++){
 //prompt user for their guess
-guess = prompt("Guess a number");
+do{
+guess = parseInt(prompt("Guess a number"));
+}while(isNaN(guess) || isPreviousGuess(guess));
 //if user guess is correct, congratulate them
 
 if (guess == number) {
@@ -17,10 +20,23 @@ if (guess == number) {
 
 //if user guess is incorrect, let them know
 else {
-    alert("incorrect. \nYou have " + (guesses - i) + " guesses left.");
+    guesses[i] = guess;
+    alert("incorrect. \nYou have guessed: " + guesses.toString() + "\nTries remaining: " + (limit - i));
+    console.log(guesses);
 }
 }
+
 //if user runs out of guesses, let them know the correct answer
 if (!won) {
-    document.write("<h1>Sorry, you lost. The correct answer was " + number + ".</h1>");
+    document.write("<h3>Sorry, you lost. The correct answer was " + number + ".</h3>");
+}
+
+//prevent duplicate guesses
+function isPreviousGuess(){
+    for(var i = 1; i < guesses.length; i++){
+        if(guesses[i] == guess){
+            return true;
+        }
+    }
+    return false;
 }
