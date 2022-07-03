b41e70d3701d889e3d305449f4ddb121c56b547f
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 14:26:58 +0300

    arrays in js

---
+++ arrays/index.js
@@ -0,0 +1,16 @@
+//arrays
+let input = ['a', 'b', 'c'];
+
+//accessing elements
+console.log(input[0]);
+
+//adding elements
+input[3] = 'd';
+
+//removing elements
+delete input[2];
+
+//iterating over arrays
+for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+}
