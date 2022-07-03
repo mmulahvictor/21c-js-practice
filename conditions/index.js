2d9be6cf559cb432ed06ee1a8a4e6b94f9274dfc
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 13:08:51 +0300

    if, else statement and comparison operators

---
+++ conditions/index.js
@@ -0,0 +1,20 @@
+//conditional statements
+//if, else
+
+//if
+if (conditon) {
+    //do something
+}
+
+//else if
+if (condition){
+    //do something
+}else(condition) =>{
+    //do something
+}
+
+//comparison operators
+//==, ===, !=, >, <, >=, <=
+//conditions test for equality(true or false)
+
+//nested if-else statements
