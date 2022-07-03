6386178677cea179be323424d73319ede912559a
mmulahvictor <mmulahvictor@gmail.com>
2022-07-03 19:42:39 +0300

    show the number of guesses remaining

--- guesing-game/index.js
+++ guesing-game/index.js
@@ -1,14 +1,18 @@
 //program data:
 let number = 4
 let guess;
+let guesses = 5;
 
+for(i = 0; i < guesses; i++){
 //prompt user for their guess
 guess = prompt("Guess a number");
 //if user guess is correct, congratulate them
 if (guess == number) {
     document.write("<h1>Correct! You won!</h1>");
+    break;
 }
 //if user guess is incorrect, let them know
 else {
-    alert("incorrect");
+    alert("incorrect. \nYou have " + (guesses - i) + " guesses left.");
+}
 }
